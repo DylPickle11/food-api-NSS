2a818df053fee84bdcb30bba2ca04f99aa530667
Dylan <Dylan.Griff11@gmail.com>
2019-08-28 16:00:04 -0500

    first commit

---
+++ src/scripts/foodapi.js
@@ -0,0 +1,26 @@
+const foodContainer= document.querySelector('.foodList');
+
+function createFoodHTML(foodObj) {
+
+    let foodHTML = `
+    <section>
+      <h3>Name:${foodObj.name}</h3>
+      <p>Category: ${foodObj.category}</p>
+      <p>Ethnicity: ${foodObj.ethnicity}</p>
+    </section>
+    `
+    return foodHTML;
+}
+
+function addFoodToDom(htmlString){
+  foodContainer.innerHTML += htmlString;
+}
+
+fetch("http://localhost:8088/food")
+    .then(foods => foods.json())
+    .then(parsedFoods => {
+        parsedFoods.forEach(item => {
+            const foodAsHTML = createFoodHTML(item)
+            addFoodToDom(foodAsHTML)
+        })
+    })
